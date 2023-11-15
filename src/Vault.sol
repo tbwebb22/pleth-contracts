657583799c8cbec3f1fe89b817ba8e1dc2525809
ortutay <marcell.ortutay@gmail.com>
2023-11-15 13:55:46 -0800

    create Vault.sol + test

--- src/Vault.sol
+++ src/Vault.sol
@@ -4,4 +4,8 @@ pragma solidity ^0.8.13;
 import { ERC1155 } from "@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
 
 contract Vault is ERC1155 {
+
+    constructor(string memory uri_) ERC1155(uri_) {
+    }
+
 }
